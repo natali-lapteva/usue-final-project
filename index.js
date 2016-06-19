ac253858c58511ea3a884ffedb8fccfd87b22edc
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2016-06-19 20:32:16 +0500

    Настраиваем автодеплой

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*
-  Copyright (c) 2008 - 2016 MongoDB, Inc. <http://mongodb.com>
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
-
 var express = require('express'),
     bodyParser = require('body-parser'),
     nunjucks = require('nunjucks'),
@@ -52,7 +35,7 @@ var ITEMS_PER_PAGE = 5;
 // Hardcoded USERID for use with the shopping cart portion
 var USERID = "558098a65133816958968d88";
 
-MongoClient.connect('mongodb://localhost:27017/mongomart', function(err, db) {
+MongoClient.connect('mongodb://root:root@ds021462.mlab.com:21462/mongomart', function(err, db) {
     "use strict";
 
     assert.equal(null, err);
